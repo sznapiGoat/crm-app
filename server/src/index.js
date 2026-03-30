079a5c5196e591aa4304d4f0f9c4c35597bade49
fstriker14 <sznapi7@seznam.cz>
2026-03-30 22:59:36 +0200

    fix cors for vercel

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173' }));
+app.use(cors({ origin: ['http://localhost:5173', 'https://crm-app-rosy-eight.vercel.app'] }))
 app.use(express.json());
 
 // Public routes
